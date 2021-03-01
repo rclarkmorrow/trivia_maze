45386c8e6eab0188214f30e5e0998913f3197fb1
Rick Morrow <rclark.morrow@gmail.com>
2021-03-01 12:06:40 -0800

    update classes, documentation

--- application/backend/game/trivia_maze_game.py
+++ application/backend/game/trivia_maze_game.py
@@ -6,38 +6,77 @@ class TriviaMazeGame:
     """
        The TriviaMazeGame class creates a Trivia Maze Game and tracks that
        game's current state so that it can saved to be played later.
+       :Param player_name: The player's name as a string.
+       :Param row_count: An integer representing the number
+                         of rows in the maze.
+       :Param col_count: An integer representing the number
+                         of columns in the maze.
     """
     def __init__(self, player_name, row_count, col_count, questions):
-        self.__maze = Maze(row_count, col_count)
+        self.__maze = Maze(row_count, col_count)  # Use a maze factory?
         self.__player = PlayerFactory.create_player(player_name)
-        self.__entrance = self.__maze.entrance_pos
-        self.__exit = self.__maze.exit_pos
-        self.__current_room = self.__entrance
+        self.__rows = row_count
+        self.__columns = col_count
+        self.__entrance = self.__maze.entrance_pos  # List coordinates
+        self.__exit = self.__maze.exit_pos  # List coordinates
+        self.__current_room = self.__entrance  # List coordinates
         self.__cheat_mode = False
         self.__questions = questions
 
     @property
     def maze(self):
+        """ Return maze as property. """
         return self.__maze
+
     @property
     def player(self):
+        """ Return player as property. """
         return self.__player
+
+    @property
+    def rows(self):
+        """ Return row count as property. """
+        return self.__rows
+
+    @property
+    def columns(self):
+        """ Return column count as property. """
+        return self.__columns
+
+    @property
+    def entrance(self):
+        """ Return maze entrance coordinates as property. """
+        return self.__entrance
+
+    @property
+    def exit(self):
+        """ Return maze exit coordinates as property. """
+        return self.__exit
+
     @property
     def current_room(self):
+        """ Return current room as property. """
         return self.__current_room
+
+    @current_room.setter
+    def current_room(self, current_room):
+        """ Set the current room to a new value. """
+        self.__current_room = current_room
+
     @property
     def cheat_mode(self):
+        """ Return cheat most enabled status as property. """
         return self.__cheat_mode
+
     @cheat_mode.setter
     def cheat_mode(self, toggle: bool):
+        """ Set cheat mode enabled status. """
         self.__cheat_mode = toggle
 
-    # def toggle_cheat(self):
-    #     # Toggle the boolean.
-    #     self.__cheat_mode = not self.__cheat_mode
-    #     # NOTE: may need to return information about whether
-    #     # the cheat mode was enable or disabled here.
-    #     return
+    @property
+    def questions(self):
+        """ Return questions list as property. """
+        return self.__questions
 
 
 if __name__ == '__main__':
